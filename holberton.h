bf5e92c4b680b5f18df02637ee2a05e228b2bb59
Logan Scanlon <scan3ls@gmail.com>
2020-03-15 05:11:48 +0000

    Add Dictionary struct and helper functions

--- holberton.h
+++ holberton.h
@@ -1,6 +1,23 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-int _printf(const char *format, ...);
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * Dictionary - pairs a handle char with a print funciton
+*/
+typedef struct Dictionary
+{
+	char handle;
+	void (*func)(va_list list);
+}Dictionary;
+
+int _putchar(char c);
+
+void _printf(const char * format, ...);
+
+void print_int(va_list list);
 
 #endif
