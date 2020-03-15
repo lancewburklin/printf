5226545284a900c8f4916ee3f14cc9c27f6670cd
Logan Scanlon <scan3ls@gmail.com>
2020-03-15 04:40:06 +0000

    Define helper function

---
+++ handles.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * print_int - prints an int
+ *@list: agrument provided to print
+ */
+
+void print_int(va_list list)
+{
+	/* Shift int to Ascii value of char */
+	int num = va_arg(list, int);
+	int negative = 0;
+	int revNum[10];
+	int index = 0;
+
+
+	if (num < 0)
+	{
+		index++;
+		negative = 1;
+		_putchar('-');
+	}
+
+	for (; num % 10 != 0; index++)
+	{
+		revNum[index] = (num % 10) > 0
+			? num % 10
+			: (num % 10) * -1;
+		num = num / 10;
+	}
+
+	for (index--; negative ? index > 0 : index >= 0; index--)
+	{
+		_putchar(revNum[index] + 48);
+	}
+}
