b528498b6f9fc0a205b4ce23d0559fbf0c705449
Logan Scanlon <scan3ls@gmail.com>
2020-03-16 19:36:58 +0000

    Fix major bugs with _printf.c Add handling for % flag
    Fixed bug when a flag is null terminated that would result in printing garbage characters

--- tests/main.c
+++ tests/main.c
@@ -7,6 +7,12 @@ int main(void)
 	char *stest = "TEST";
 
 	/* %d and %i test */
-	_printf("no1: %d no2: %% string: %s\n", DTEST, "yes", stest);
+	_printf("no1: %d\n", DTEST);
+	_printf("no2: %i\n", ITEST);
+	_printf("no3: %s\n", stest);
+	_printf("no4: %c\n", 'c');
+	_printf("no5: %%\n");
+	_printf("-----------------------\n");
+	_printf("no1: %d\nno2: %i\nno3: %s\nno4: %c\nno5: %%\n", DTEST, ITEST, stest, 'c');
 	return 0;
 }
