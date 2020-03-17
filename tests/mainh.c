ac0810314320af375d42248526850f49d2d559fa
lancewburklin <1677@holbertonschool.com>
2020-03-17 04:37:43 +0000

    Added to test file

--- tests/mainh.c
+++ tests/mainh.c
@@ -14,18 +14,14 @@ int main(void)
 	unsigned int ui;
 	void *addr;
 
-	len = _printf("%s: %c: %", NULL, NULL);
-	printf("\n");
-	len2 = printf("%s: %c: %", NULL, NULL);
-	printf("\n");
+	len = _printf("Let's try to printf a simple sentence.\n");
+	len2 = printf("Let's try to printf a simple sentence.\n");
 	ui = (unsigned int)INT_MAX + 1024;
 	addr = (void *)0x7ffe637541f0;
-	_printf("Length: [%d, %i]\n", len, len);
-	printf("Length2:[%d, %i]\n", len2, len2);
-	len = _printf("Negative:[%d]\n", -762534);
-	len2 = printf("Negative:[%d]\n", -762534);
-	_printf("Len:[%d]\n", len);
-	_printf("Len:[%d]\n", len2);
+	_printf("Length:[%d, %i]\n", len, len);
+	printf("Length:[%d, %i]\n", len2, len2);
+	_printf("Negative:[%d]\n", -762534);
+	printf("Negative:[%d]\n", -762534);
 	_printf("Unsigned:[%u]\n", ui);
 	printf("Unsigned:[%u]\n", ui);
 	_printf("Unsigned octal:[%o]\n", ui);
@@ -42,9 +38,16 @@ int main(void)
 	len2 = printf("Percent:[%%]\n");
 	_printf("Len:[%d]\n", len);
 	printf("Len:[%d]\n", len2);
-	len = _printf("Unknown:[%r]\n");
-	len2 = printf("Unknown:[%r]\n");
-	_printf("Len:[%d]\n", len);
-	_printf("Len:[%d]\n", len2);
+	_printf("Unknown:[%r]\n");
+	printf("Unknown:[%r]\n\n");
+
+	_printf("%k\n");
+	printf("%k\n");
+	_printf(NULL);
+	printf(NULL);
+	_printf("%\n");
+	printf("%\n");
+	_printf("%%\n");
+	printf("%%\n");
 	return (0);
 }
